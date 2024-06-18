2b8bd1b9bdf16281edf4aca00ce2c3253842c681
raagh <rk00968@surrey.ac.uk>
2024-06-18 19:01:12 +0530

    Order of Operations

--- main.cpp
+++ main.cpp
@@ -1,10 +1,13 @@
 #include <iostream>
 
 int main() {
-    int x = 10;
-    int y = x++;      // x = 11, y = 10
-    int z = ++x;      // x = 11, y = 11 (when above line commented)
-    // when ++(increment) is applied before a variable then it will increase the value of the variable the increment is applied to and store it as well as store the newly incremented value in the newly initiated variable
-    std::cout << x;
+    // () has highest priority
+    // * and / have higher priority than
+    // + and -
+    double x = 10;
+    double y = 5;
+    double z = 0;
+    z = (x+10)/(3*y);
+    std::cout << z;
     return 0;
 }
