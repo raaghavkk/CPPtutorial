6ce6d611d021f7fd6494fe89e69c03d7fdd52582
raagh <rk00968@surrey.ac.uk>
2024-06-18 19:14:34 +0530

    Printing to console part 1

--- main.cpp
+++ main.cpp
@@ -1,13 +1,18 @@
 #include <iostream>
 
 int main() {
-    // () has highest priority
-    // * and / have higher priority than
-    // + and -
-    double x = 10;
-    double y = 5;
-    double z = 0;
-    z = (x+10)/(3*y);
-    std::cout << z;
+    int x = 10;
+    int y = 20;
+    int z = 5;
+    // standard output stream - std::cout - stream means a sequence of characters
+    // << these double angled brackets are the stream insertion operator - it's an operator to insert something to our output stream
+    std::cout << "x = ";
+    std::cout << x << std::endl;
+    // we can chain multiple stream insertion operators as shown below
+    std::cout << "y = " << y << std::endl << "-----------" << std::endl;
+    // we can combine this information to create a simplified print statement like this :
+    std::cout << "x = " << x << std::endl
+              << "y = " << y << std::endl
+              << "z = " << z;
     return 0;
 }
