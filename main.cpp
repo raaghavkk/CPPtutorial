3dd8c439451f144c34ba6c8a9f4746dbcf174aaf
raagh <rk00968@surrey.ac.uk>
2024-06-18 12:50:59 +0530

    Switching variable values (Bucket analogy)

--- main.cpp
+++ main.cpp
@@ -1,8 +1,16 @@
 #include <iostream>
 
+using namespace std;
+
 int main() {
-    int file_size = 0;
-    double sales = 9.99;
-    std::cout << file_size;
+    int a = 1;
+    int b = 2;
+    int temp = a;
+
+    a = b;
+    b = temp;
+
+    cout <<a;
+
     return 0;
 }
