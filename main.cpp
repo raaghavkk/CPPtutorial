a4e41ac3064697e9d7ccdbb02b7629656afb1679
raagh <rk00968@surrey.ac.uk>
2024-06-18 18:54:05 +0530

    Mathematical Expressions

--- main.cpp
+++ main.cpp
@@ -1,9 +1,10 @@
 #include <iostream>
 
 int main() {
-    int file_size; // Snake Case
-    int FileSize; // PascalCase - for naming classes
-    int fileSize; // CamelCase - for naming variables and constants
-    int iFileSize; // Hungarian Notation
+    int x = 10;
+    int y = x++;      // x = 11, y = 10
+    int z = ++x;      // x = 11, y = 11 (when above line commented)
+    // when ++(increment) is applied before a variable then it will increase the value of the variable the increment is applied to and store it as well as store the newly incremented value in the newly initiated variable
+    std::cout << x;
     return 0;
 }
