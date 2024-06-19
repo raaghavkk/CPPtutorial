70fff879653c7175fb4a7c3347520fdedca36e42
raagh <rk00968@surrey.ac.uk>
2024-06-19 10:52:29 +0530

    Fahrenheit to Celsius

--- main.cpp
+++ main.cpp
@@ -3,19 +3,10 @@
 using namespace std;
 
 int main() {
-    double sales = 95000;
-    cout << "Sales: $" << sales << endl;
-
-    const double stateTaxRate = 0.04;
-    double stateTax = sales * stateTaxRate;
-    cout << "State Tax: $" << stateTax << endl;
-
-    const double countyTaxRate = 0.02;
-    double countyTax = sales * countyTaxRate;
-    cout << "County Tax: $" << countyTax << endl;
-
-    double totalTax = stateTax + countyTax;
-    cout << "Total Tax: $" << totalTax;
-
+    cout << "Enter Fahrenheit temperature: " << endl;
+    double f;
+    cin >> f;
+    double c = (f - 32) / 1.8;
+    cout << "The Temperature in degrees celsius is: " << c;
     return 0;
 }
