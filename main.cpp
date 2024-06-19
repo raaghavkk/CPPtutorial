6b8c0a4e578a8425b55e661fb6509a3ac92b0ce4
raagh <rk00968@surrey.ac.uk>
2024-06-19 15:13:10 +0530

    Narrowing

--- main.cpp
+++ main.cpp
@@ -1,13 +1,10 @@
 #include <iostream>
-#include <cmath>
 
 using namespace std;
 
 int main() {
-    /* since we are using a brace initialiser, it will not compile the incorrect data associated to the wrong data type
-     *normal initialiser is '=' where we would assign a integer variable like int number = 1.2;
-     */
-    int number {1.2};
-    cout << number;
+    int number = 1'000'000;
+    short another = number;
+    cout << another;
     return 0;
 }
